239e261a75623445b7e1660bad7ed0760c8f81ac
Rik Bose <rbose@cs.rochester.edu>
2018-11-08 00:05:20 -0500

    finding packages

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, findpackages
 
 setup(name="trips-web",
         version='1.0.0',
+        name='tripsweb'
         description='Command-line interface for the TRIPS web parser',
         author="Rik Bose",
         author_email="rbose@cs.rochester.edu",
         url="http://www.github.com/mrmechko/trips-web",
+        packages=find_packages(),
         entry_points = {
             "console_scripts": ['trips-web=tripsweb.query:main'],
         },
